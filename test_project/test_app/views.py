7fb5502254d95419549b4d6c282b5abb7040bc6c
Andrei Coman <comandrei@gmail.com>
2022-07-14 19:59:24 +0300

    Sample view

--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def hello(request):
+    context = {
+        'ana': 'are mere'
+    }
+    return render(request, "hello.html", context)
